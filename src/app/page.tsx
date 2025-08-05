07cf4c2fb94c57ed0068889b3c345d9b4f75c574
lucasbars <darklucassilva@gmail.com>
2025-08-05 14:09:08 -0300

    updated app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Imc from "./imc/page";
 
+export const metadata = {
+  title: "Calculadora de IMC",
+  description:
+    "Veja seu Índice de Massa Corporal com esta calculadora simples.",
+};
 export default function Home() {
   return <Imc />;
 }
